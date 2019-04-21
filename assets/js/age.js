7050b58d876819702cfd10e5722a22bab59f4acb
Emmanuel Galland <emmanuel.galland117@gmail.com>
2019-04-21 13:41:16 +0200

    append

--- assets/js/age.js
+++ assets/js/age.js
@@ -1,5 +1,5 @@
-$(function(){
-    $('#send').click(function(){
+$(() =>{
+    $('#send').click(()=>{
         if($('.datepicker')[0].value !== ''){
             const birthdate = $('.datepicker')[0].value;
             const bYear = birthdate.slice(6,10);
@@ -18,112 +18,41 @@ $(function(){
                 const saturnAge = (dayAge / 10759).toFixed(2);
                 const uranusAge = (dayAge / 30685).toFixed(2);
                 const neptuneAge = (dayAge / 60266).toFixed(2);
-                console.table([
-                    ['Age sur Mercure', mercuryAge],
-                    ['Age sur Venus', venusAge],
-                    ['Age sur Terre', earthAge],
-                    ['Age sur Mars', marsAge],
-                    ['Age sur Jupiter', jupiterAge],
-                    ['Age sur Saturne', saturnAge],
-                    ['Age sur Uranus', uranusAge],
-                    ['Age sur Neptune', neptuneAge]
-                ]);
+                const solarSys = [
+                    ['mercury', 'Mercure'],
+                    ['venus' , 'Venus'],
+                    ['earth' , 'Terre'],
+                    ['mars' , 'Mars'],
+                    ['jupiter' , 'Jupiter'],
+                    ['saturn' , 'Saturne'],
+                    ['uranus' , 'Uranus'],
+                    ['neptune' , 'Neptune']
+                ];
                 $('#test').remove();
-                $('body').append(`
-                    <div id="test">
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="assets/images/mercury_full.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title activator grey-text text-darken-4">Mercure<i class="material-icons right">more_vert</i></span>
-                                <p>Votre âge sur Mercure est de ${mercuryAge} an${mercuryAge > 1? 's' : ''}.</p>
-                            </div>
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Mercure</span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
+                $('body').append(`<div id="test"></div>`);
+                solarSys.forEach((solarSysArray,index) =>{
+
+                    $('#test').append(`
+                    ${index % 4 == 0? '<div class="row">' : ''}
+                    <div class="card col s12 m6 l3">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="assets/images/${solarSysArray[0]}_full.jpg">
                         </div>
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="assets/images/venus_full.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title activator grey-text text-darken-4">Venus<i class="material-icons right">more_vert</i></span>
-                                <p>Votre âge sur Mercure est de ${venusAge} an${venusAge > 1? 's' : ''}.</p>
-                            </div>
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Mercure</span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">${solarSysArray[1]}<i class="material-icons right">more_vert</i></span>
+                            <p>Votre âge sur ${solarSysArray[1]} est de ${eval(solarSysArray[0]+'Age')} an${eval(solarSysArray[0]+'Age') > 1? 's' : ''}.</p>
                         </div>
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="assets/images/earth_full.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title activator grey-text text-darken-4">Terre<i class="material-icons right">more_vert</i></span>
-                                <p>Votre âge sur Mercure est de ${earthAge} an${earthAge > 1? 's' : ''}.</p>
-                            </div>
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Mercure</span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="assets/images/mars_full.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title activator grey-text text-darken-4">Mars<i class="material-icons right">more_vert</i></span>
-                                <p>Votre âge sur Mercure est de ${marsAge} an${marsAge > 1? 's' : ''}.</p>
-                            </div>
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Mercure</span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="assets/images/jupiter_full.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title activator grey-text text-darken-4">Jupiter<i class="material-icons right">more_vert</i></span>
-                                <p>Votre âge sur Mercure est de ${jupiterAge} an${jupiterAge > 1? 's' : ''}.</p>
-                            </div>
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Mercure</span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="assets/images/uranus_full.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title activator grey-text text-darken-4">Uranus<i class="material-icons right">more_vert</i></span>
-                                <p>Votre âge sur Mercure est de ${uranusAge} an${uranusAge > 1? 's' : ''}.</p>
-                            </div>
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Mercure</span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="assets/images/neptune_full.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title activator grey-text text-darken-4">Neptune<i class="material-icons right">more_vert</i></span>
-                                <p>Votre âge sur Mercure est de ${neptuneAge} an${neptuneAge > 1? 's' : ''}.</p>
-                            </div>
-                            <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Mercure</span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                            </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>${solarSysArray[1]}</span>
+                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
                         </div>
                     </div>
-                `);
+                    ${(index+1) % 4 == 0? '</div>' : ''}`
+                    );
+                    console.log(index % 4 == 0? 'row': 'pas row');
+                    console.log((index+1) % 4 == 0 ? 'fin row' : 'no fin row');
+                });
+
             }else{
                 alert(`La date doit être antérieur à aujourd'hui`);
             }
