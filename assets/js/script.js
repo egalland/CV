d4f2c74185c511d387a9622c99f64e4a5058e137
Duden <kwyjibo117@gmail.com>
2019-05-08 02:07:41 +0200

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
 $(()=> {
     $('.parallax').parallax();
     $('.collapsible').collapsible();
+    
+    var options = [
+      {
+        selector: '#projects',
+        offset: 100,
+        callback: function(el) {
+          Materialize.showStaggeredList($(el));
+        }
+      },
+      {
+        selector: '#skills',
+        offset: 100,
+        callback: function(el) {
+          Materialize.showStaggeredList($(el));
+        }
+      }
+    ];
+      
+      Materialize.scrollFire(options);
 });
